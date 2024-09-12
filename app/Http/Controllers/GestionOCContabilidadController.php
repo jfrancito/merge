7457320c00bf3ff8006a1cad3d186b5b24f33c89
Jorge Francelli <francito20al@gmail.com>
2024-09-12 15:36:27 -0500

    usuario de cdr

--- app/Http/Controllers/GestionOCContabilidadController.php
+++ app/Http/Controllers/GestionOCContabilidadController.php
@@ -451,6 +451,9 @@ class GestionOCContabilidadController extends Controller
         }
         else{
 
+            $prefijocarperta        =   $this->prefijo_empresa($ordencompra->COD_EMPR);
+            $lecturacdr             =   $this->lectura_cdr_archivo($idoc,$this->pathFiles,$prefijocarperta,$ordencompra->NRO_DOCUMENTO_CLIENTE);
+
             $detalleordencompra     =   $this->con_lista_detalle_comprobante_idoc_actual($idoc);
             $detallefedocumento     =   FeDetalleDocumento::where('ID_DOCUMENTO','=',$idoc)->where('DOCUMENTO_ITEM','=',$fedocumento->DOCUMENTO_ITEM)->get();
 
@@ -717,6 +720,10 @@ class GestionOCContabilidadController extends Controller
         
         }
         else{
+
+            //lectura del cdr
+            $prefijocarperta        =   $this->prefijo_empresa($ordencompra->COD_EMPR);
+            $lecturacdr             =   $this->lectura_cdr_archivo($idoc,$this->pathFiles,$prefijocarperta,$ordencompra->NRO_DOCUMENTO_CLIENTE);
 
             $detalleordencompra     =   $this->con_lista_detalle_contrato_comprobante_idoc($idoc);
             $detallefedocumento     =   FeDetalleDocumento::where('ID_DOCUMENTO','=',$idoc)->where('DOCUMENTO_ITEM','=',$fedocumento->DOCUMENTO_ITEM)->get();
