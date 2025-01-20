e027acb58d090fc09f590f8a77ab1b81a2687039
Jorge Francelli <francito20al@gmail.com>
2025-01-20 13:18:14 -0500

    CAMBIOS ESTIBa permiso 73

--- resources/views/comprobante/form/formaprobarcontestiba.blade.php
+++ resources/views/comprobante/form/formaprobarcontestiba.blade.php
@@ -36,7 +36,7 @@
 </div>
 <div class="row">
   <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
-    @include('comprobante.form.contrato.archivosobservados')
+    @include('comprobante.form.estiba.archivosobservados')
   </div>
 </div>
 
