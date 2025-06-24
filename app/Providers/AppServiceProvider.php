89f4749f8922371b98e66d68d460aa46df6ed80b
Jorge Francelli <francito20al@gmail.com>
2025-06-24 12:47:48 -0500

    cambios de lq personal 05

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
         View::share('capeta', '/merge');
 
 
-        View::share('version', '7.30');
+        View::share('version', '7.35');
 
 
         View::share('titulo', '');
