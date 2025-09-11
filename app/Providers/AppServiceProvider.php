37e9a93bb2183b2f3f7d07f245ccf5afee0fce32
FabianC19900 <fllontopcr@unprg.edu.pe>
2025-09-11 17:01:00 -0500

    vale detalle

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
         View::share('capeta', '/merge');
 
 
-        View::share('version', '9.95');
+        View::share('version', '9.97');
 
         View::share('titulo', '');
 
