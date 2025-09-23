dd1de0503404731182cabfc541ae18019fbece8a
Kiri2801 <68066885+Kiri2801@users.noreply.github.com>
2025-09-23 12:29:10 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
         View::share('capeta', '/merge');
 
 
-        View::share('version', '9.98');
+        View::share('version', '9.99');
 
         View::share('titulo', '');
 
