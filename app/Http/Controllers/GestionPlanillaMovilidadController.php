a437c1a1e94a2ea3625e428f5d77cfcc2c7ca8b6
Jorge Francelli <francito20al@gmail.com>
2025-10-21 15:04:07 -0500

    commit de prueba para forzar 13

--- app/Http/Controllers/GestionPlanillaMovilidadController.php
+++ app/Http/Controllers/GestionPlanillaMovilidadController.php
@@ -1489,7 +1489,9 @@ class GestionPlanillaMovilidadController extends Controller
             if (Session::get('usuario')->id == '1CIX00000380') {
                 $centro_id = 'CEN0000000000002';
             }
-
+            if (Session::get('usuario')->id == '1CIX00000391') {
+                $centro_id = 'CEN0000000000002';
+            }
 
 
             $periodo        =   $this->gn_periodo_actual_xanio_xempresa($anio, $mes, Session::get('empresas')->COD_EMPR);
