ed682ff55f5d5df76ba44518161b2e7e872610b9
Jorge Francelli <francito20al@gmail.com>
2025-12-26 14:14:08 -0500

    commit de prueba para forzar 103

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         View::share('capeta', '/merge');
 
-        View::share('version', '12.31');
+        View::share('version', '12.32');
 
         View::share('titulo', '');
 
