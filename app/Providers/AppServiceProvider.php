d90f23bf119b324a5d1d746498c5d78b366da892
Kiri2801 <68066885+Kiri2801@users.noreply.github.com>
2025-12-26 12:10:27 -0500

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         View::share('capeta', '/merge');
-        View::share('version', '12.27');
+        View::share('version', '12.28');
         View::share('titulo', '');
 
         Validator::extend('unico', function($attribute, $value, $parameters , $validator){
