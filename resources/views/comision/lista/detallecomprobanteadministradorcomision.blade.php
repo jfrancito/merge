f17fe2850fb1d4442e40b7202c3e6890d599a1e7
Jorge Francelli <francito20al@gmail.com>
2026-01-08 07:46:28 -0500

    commit de prueba para forzar 110

--- resources/views/comision/lista/detallecomprobanteadministradorcomision.blade.php
+++ resources/views/comision/lista/detallecomprobanteadministradorcomision.blade.php
@@ -49,7 +49,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 @if($fedocumento->OPERACION_DET == 'SIN_XML') ocultar @endif" >
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4" >
                 <div class="panel panel-default panel-contrast">
                     <div class="panel-heading" style="background: #1d3a6d;color: #fff;">CONSULTA API SUNAT
                     </div>
