b1b00fef730f1ecba52298c0b8aae01a1fd21cef
Jorge Francelli <francito20al@gmail.com>
2026-04-27 10:05:02 -0500

    primer commit 10010

--- public/js/comprobante/uc.js
+++ public/js/comprobante/uc.js
@@ -115,6 +115,24 @@ $(document).ready(function () {
 
     });
 
+
+
+    $(".asientomodelo").on('click','.buscardocumentoadmin', function() {
+
+        event.preventDefault();
+
+        var operacion_id            =   $('#operacion_id').val();
+        var idopcion                =   $('#idopcion').val();
+        var _token                  =   $('#token').val();
+
+        data            =   {
+                                _token                  : _token,
+                                operacion_id            : operacion_id,
+                                idopcion                : idopcion
+                            };
+        ajax_normal(data,"/ajax-buscar-documento-gestion-uc");
+
+    });
     $("#tipo_cambio_asiento_reparable").on('change', function (e) {
 
         let moneda = $('#moneda_asiento_reparable').val();
