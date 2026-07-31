a6ff57c41c47a1a2f6220fd2942914879a94820a
FabianC19900 <fllontopcr@unprg.edu.pe>
2026-07-31 09:16:34 -0500

    Vale a rendir

--- public/js/comprobante/registro.js
+++ public/js/comprobante/registro.js
@@ -921,7 +921,6 @@ $(document).ready(function(){
         var tarchivos = JSON.parse(tarchivosDataEl.textContent);
         tarchivos.forEach(function(item) {
             var options = {
-                theme: 'fa5',
                 language: 'es',
                 allowedFileExtensions: [item.formato]
             };
